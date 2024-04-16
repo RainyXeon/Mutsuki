76da5498b9c1bec863b94f4495949ee35f1dee36
Vinn <me@vinn.my.id>
2024-04-17 03:00:14 +0700

    Fix Interaction.ts
    Cannot find module './Command.js' or its corresponding type declarations.
    
    Signed-off-by: Vinn <me@vinn.my.id>

--- src/@types/Interaction.ts
+++ src/@types/Interaction.ts
@@ -9,7 +9,7 @@ import {
 } from "discord.js";
 import { Manager } from "../manager.js";
 import { ApplicationCommandOptionType } from "discord-api-types/v10";
-import { Accessableby } from "./Command.js";
+import { Accessableby } from "../structures/Command.js";
 
 export type GlobalInteraction =
   | CommandInteraction
